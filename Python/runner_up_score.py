b9c59cefebcacfd75af6fab202ab2d8f9e175b52
mersihakaramustafic <karamustafic.mersiha@gmail.com>
2024-12-17 00:03:52 +0100

    Adding comment to the task

--- Python/runner_up_score.py
+++ Python/runner_up_score.py
@@ -2,6 +2,7 @@ if __name__ == '__main__':
     n = int(input())
     arr = map(int, input().split())
     
+#dictionaries don't have duplicated records
 mylist = list(dict.fromkeys(arr))
 mylist.sort()
 print(mylist[len(mylist) - 2])
