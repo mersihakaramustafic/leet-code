367244e6079c330af04e80f5724f6bc6adc85170
mersihakaramustafic <karamustafic.mersiha@gmail.com>
2025-07-21 13:38:16 +0200

    Adding Upper function for hex format

--- Python/string_formatting.py
+++ Python/string_formatting.py
@@ -1,7 +1,7 @@
 def print_formatted(number):
     width = len(bin(number)[2:])
     for i in range(number):
-        print(str(i+1).rjust(width) + " " + oct(i+1)[2:].rjust(width) + " " + hex(i+1)[2:].rjust(width) + " " + bin(i+1)[2:].rjust(width))
+        print(str(i+1).rjust(width) + " " + oct(i+1)[2:].rjust(width) + " " + hex(i+1)[2:].upper().rjust(width) + " " + bin(i+1)[2:].rjust(width))
 
 if __name__ == '__main__':
     n = int(input())
